67a60e70cc9c04cd35597c35cde96f951346fb3f
Daan Huybrechs <daan.huybrechs@kuleuven.be>
2022-11-01 19:08:13 +0100

    normalize A

--- src/fourier_ext2D.jl
+++ src/fourier_ext2D.jl
@@ -18,15 +18,14 @@ function FourierExtension2(f, Ω, n::Int; tol = 1e-8, oversamp::Real = 2.0)
         indsx, indsy = grid_filter(xgrid, ygrid, Ω)
         M = length(indsx)
     end
-
-    b = complex([f(xgrid[indsx[k]],ygrid[indsy[k]]) for k = 1:M])
+    b = complex([f(xgrid[indsx[k]],ygrid[indsy[k]]) for k = 1:M])/L
     rank_guess = min(round(Int, 5*sqrt(N)*log10(N))+10, div(N,2))
     padded_tensor = Array{eltype(b),3}(undef, L, L, rank_guess)
-    bfftplan = plan_bfft!(padded_tensor,1:2) # note that fft = L * conj ∘ bfft ∘ conj
+    bfftplan = plan_bfft!(padded_tensor[:,:,1]) # note that fft = L * conj ∘ bfft ∘ conj
     A = LinearMap(x -> fourier_ext_2D_A!(x,n,indsx,indsy,L,bfftplan,padded_tensor), y -> fourier_ext_2D_Astar!(y,n,indsx,indsy,L,bfftplan,padded_tensor), M, N)
 
     coeffs = AZ_algorithm(A, A, b, rank_guess=rank_guess, tol=tol, maxiter=100) # Z = A for Fourier extensions
-    return FourierExtension2(Ω, reshape(coeffs,2n+1,2n+1)), A, b
+    FourierExtension2(Ω, reshape(coeffs,2n+1,2n+1)), A, b
 end
 
 # Adaptive Constructor
@@ -43,37 +42,46 @@ function FourierExtension2(f, Ω; tol=1e-8, oversamp::Real = 2.0, nmin::Int=8, n
         if norm(F.coeffs)/fvalsnorm < 100
             if norm(abs.(fvals - vals))/fvalsnorm < 10*tol
                 if abs(F(0.51,0.49) - fval) < 10*tol
-                    return F
+                    F
                 end
             end
         end
         n *= 2
     end
     @warn "Maximum number of coefficients "*string(length(F.coeffs))*" reached in constructing FourierExtension2. Try setting nmax higher."
-    return F
+    F
 end
 
-function fourier_ext_2D_A!(c, n::Int, indsx::Vector{Int64}, indsy::Vector{Int64}, L::Int, bfftplan, padded_tensor::AbstractArray{T}) where T
-    r = size(c,2)
-    c = reshape(c, 2n+1, 2n+1, r)
-    padded_tensor[:,:,1:r] = [c[n+1:2n+1,n+1:2n+1,:] zeros(T,n+1,L-(2n+1),r) c[n+1:2n+1,1:n,:];
-                                                     zeros(T,L-(2n+1),L,r);
-                              c[1:n,n+1:2n+1,:]      zeros(T,n,L-(2n+1),r)   c[1:n,1:n,:]]
-    bfftplan*padded_tensor
-    vals = [padded_tensor[indsx[k],indsy[k],j] for k=1:length(indsx), j=1:r]
-    return vals
-end
-
-function fourier_ext_2D_Astar!(v, n::Int, indsx::Vector{Int64}, indsy::Vector{Int64}, L::Int, bfftplan, padded_tensor::AbstractArray{T}) where T
-    r = size(v,2)
-    padded_tensor[:,:,1:r] .= zero(T)
-    for k = 1:length(indsx)
-        padded_tensor[indsx[k],indsy[k],1:r] = conj.(v[k,:])
+function fourier_ext_2D_A!(coef, n::Int, indsx::Vector{Int64}, indsy::Vector{Int64}, L::Int, bfftplan, padded_tensor::AbstractArray)
+    r = size(coef,2)
+    c = reshape(coef, 2n+1, 2n+1, r)
+    for i in 1:r
+        @views padded_tensor[:,:,i] .= 0
+        @views padded_tensor[1:n+1,1:n+1,i] = c[n+1:2n+1,n+1:2n+1,i]
+        @views padded_tensor[1:n+1,L-n+1:L,i] = c[n+1:2n+1,1:n,:]
+        @views padded_tensor[L-n+1:L,1:n+1,i] = c[1:n,n+1:2n+1,:]
+        @views padded_tensor[L-n+1:L,L-n+1:L,i] = c[1:n,1:n,:]
     end
-    bfftplan*padded_tensor 
+    for i in 1:r
+        bfftplan*view(padded_tensor, :, :, i)
+    end
+    [padded_tensor[indsx[k],indsy[k],j]/L for k=1:length(indsx), j=1:r]
+end
+
+function fourier_ext_2D_Astar!(v, n::Int, indsx::Vector{Int64}, indsy::Vector{Int64}, L::Int, bfftplan, padded_tensor::AbstractArray)
+    r = size(v,2)
+    padded_tensor[:,:,1:r] .= zero(eltype(padded_tensor))
+    for k = 1:length(indsx)
+        for i in 1:r
+            padded_tensor[indsx[k],indsy[k],i] = conj(v[k,i])
+        end
+    end
+    for i in 1:r
+        bfftplan*view(padded_tensor, :, :, i)
+    end
     coeffs = conj.([padded_tensor[L-n+1:L,L-n+1:L,1:r] padded_tensor[L-n+1:L,1:n+1,1:r];
                     padded_tensor[1:n+1,L-n+1:L,1:r]   padded_tensor[1:n+1,1:n+1,1:r]])
-    return reshape(coeffs, (2n+1)^2, r)
+    reshape(coeffs, (2n+1)^2, r)/L
 end
 
 function grid_filter(xgrid::AbstractVector,ygrid::AbstractVector,Ω)
@@ -89,7 +97,7 @@ function grid_filter(xgrid::AbstractVector,ygrid::AbstractVector,Ω)
             end
         end
     end
-    return indsx, indsy
+    indsx, indsy
 end
 
 function evaluate(F::FourierExtension2,x,y)
@@ -111,8 +119,7 @@ function evaluate(F::FourierExtension2,x,y)
         end
         vals = vals.*zx + innervals
     end
-    vals = vals.*exp.(-π*im*(x*(Nx-1) + y*(Ny-1)))
-    return vals
+    vals.*exp.(-π*im*(x*(Nx-1) + y*(Ny-1)))
 end
 
 (F::FourierExtension2)(x,y) = evaluate(F,x,y)
@@ -138,7 +145,7 @@ function grid_evaluate(F::FourierExtension2{T}, Ω, L::Int) where T
     ifft!(paddedmat)
     indsx, indsy = grid_filter(0:1/L:1L, 0:1/L:1, Ω)
     vals = [paddedmat[indsx[k],indsy[k]] for k = 1:length(indsx)] * L^2
-    return indsx, indsy, vals
+    indsx, indsy, vals
 end
 
 
