69fb3485108e0ea773823b855fb067e3a1a85b18
Marcus Webb <marcusdavidwebb@gmail.com>
2022-11-02 17:05:56 +0000

    Example 2D updated with timing plot

--- examples/example2D.jl
+++ examples/example2D.jl
@@ -1,7 +1,24 @@
-using FourierExtensions
+using Revise
+using FourierExtensions, Plots
 f = (x,y) -> exp(x) + y^2 + x - cos(y)
 Ω = (x,y) -> (x-.5)^2 + (y-.5)^2 < 0.2
-n = (12,15)
-@time F, A, b = FourierExtension2(f,Ω, n);
-using Plots
-@time contourf(F,(100,120))
+n = (20,22)
+@time F = FourierExtension2(f,Ω, n);
+abs(F(0.51,0.48)-f(0.51,0.48))
+
+contourf(F,(1000,1200))
+
+# Timings
+
+# @time F = FourierExtension2(f,Ω, (6,6));
+# @time F = FourierExtension2(f,Ω, (12,12));
+# @time F = FourierExtension2(f,Ω, (24,24));
+# @time F = FourierExtension2(f,Ω, (48,48));
+# @time F = FourierExtension2(f,Ω, (96,96));
+
+scatter([6^2;12^2;24^2;48^2;96^2],
+  [0.007255;0.045697;0.480478;8.364671;128.381058],
+  xscale=:log10, yscale=:log10, label="Timings",legend=:topleft)
+  plot!([6^2;12^2;24^2;48^2;96^2], 1e-6*[6^2;12^2;24^2;48^2;96^2], label="O(N)")
+  plot!([6^2;12^2;24^2;48^2;96^2], 1e-6*[6^2;12^2;24^2;48^2;96^2].^2, label="O(N^2)")
+plot!([6^2;12^2;24^2;48^2;96^2], 1e-6*[6^2;12^2;24^2;48^2;96^2].^3, label="O(N^3)")
