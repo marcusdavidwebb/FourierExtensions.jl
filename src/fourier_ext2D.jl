8759c6750f40545f27e7ebb7da5f38c2a924a86a
Marcus Webb <marcusdavidwebb@gmail.com>
2022-11-02 16:26:51 +0000

    Refinement of 2D code

--- src/fourier_ext2D.jl
+++ src/fourier_ext2D.jl
@@ -3,29 +3,29 @@ struct FourierExtension2{T}
     coeffs :: Matrix{T}
 end
 
-function FourierExtension2(f, Ω, n; tol = 1e-8, oversamp = 2)
-    N = (2n[1]+1)*(2n[2]+1)
+function FourierExtension2(f, Ω, n; tol = 1e-12, oversamp = 2)
     L = ceil.(Int, 2oversamp.*n)
-    x_grid, y_grid, gridrefs = grid_mask(L, Ω)
-    while length(gridrefs) < oversamp*N
+    grid, gridΩrefs = grid_mask(Ω, L)
+    N = (2n[1]+1)*(2n[2]+1)
+    while length(gridΩrefs) < oversamp*N # try to ensure oversampling rate
         L .*= 2
-        x_grid, y_grid, gridrefs = grid_mask(L, Ω)
+        grid, gridΩrefs = grid_mask(Ω, L)
     end
-    b = complex(f.(x_grid,y_grid')[gridrefs]/prod(L))
+    b = complex(f.(grid[1], grid[2]')[gridΩrefs]/prod(L))
     M = length(b)
     padded_data = Matrix{eltype(b)}(undef, L)
     ifftplan! = plan_ifft!(padded_data)
     fftplan! = plan_fft!(padded_data)
     A = LinearMap(
-        (output,x) -> fourier_ext_2D_A!(output, x, n, gridrefs, L, ifftplan!, padded_data),
-        (output,y) -> fourier_ext_2D_Astar!(output, y, n, gridrefs, L, fftplan!, padded_data),
+        (output,x) -> fourier_ext_2D_A!(output, x, n, gridΩrefs, L, ifftplan!, padded_data),
+        (output,y) -> fourier_ext_2D_Astar!(output, y, n, gridΩrefs, L, fftplan!, padded_data),
         M, N; ismutating=true)
-    rank_guess = min(round(Int, 5*sqrt(N)*log10(N))+10, div(N,2))
+    rank_guess = min(round(Int, 3*sqrt(N)*log10(N))+20, div(N,2))
     coeffs = AZ_algorithm(A, A, b; rank_guess, tol) # Z = A for Fourier extensions
-    FourierExtension2(Ω, reshape(coeffs, 2n[1]+1, 2n[2]+1)), A, b
+    FourierExtension2(Ω, reshape(coeffs, 2n[1]+1, 2n[2]+1))
 end
 
-function fourier_ext_2D_A!(output, coef, n, gridrefs, L, ifftplan!, padded_data)
+function fourier_ext_2D_A!(output, coef, n::Tuple{Int,Int}, gridΩrefs, L::Tuple{Int,Int}, ifftplan!, padded_data)
     nx, ny = n
     Lx, Ly = L
     c = reshape(coef, 2nx+1, 2ny+1)
@@ -35,15 +35,15 @@ function fourier_ext_2D_A!(output, coef, n, gridrefs, L, ifftplan!, padded_data)
     @views padded_data[Lx-nx+1:Lx,1:ny+1] = c[1:nx,ny+1:2ny+1]
     @views padded_data[Lx-nx+1:Lx,Ly-ny+1:Ly] = c[1:nx,1:ny]
     ifftplan!*padded_data
-    @views output .= padded_data[gridrefs]
+    @views output .= padded_data[gridΩrefs]
     output
 end
 
-function fourier_ext_2D_Astar!(output, v, n, gridrefs, L, fftplan!, padded_data)
+function fourier_ext_2D_Astar!(output, v, n::Tuple{Int,Int}, gridΩrefs, L::Tuple{Int,Int}, fftplan!, padded_data)
     nx, ny = n
     Lx, Ly = L
     padded_data .= 0
-    @views padded_data[gridrefs] = v
+    @views padded_data[gridΩrefs] = v
     fftplan!*padded_data
     d = reshape(output, 2nx+1, 2ny+1)
     @views d[1:nx,1:ny] = padded_data[Lx-nx+1:Lx,Ly-ny+1:Ly]
@@ -53,40 +53,37 @@ function fourier_ext_2D_Astar!(output, v, n, gridrefs, L, fftplan!, padded_data)
     output
 end
 
-function grid_mask(L, Ω)
-    x_grid = (0:L[1]-1)/L[1]
-    y_grid = (0:L[2]-1)/L[2]
-    Z = Ω.(x_grid,y_grid')
-    gridrefs = findall(Z)
-    x_grid, y_grid, gridrefs
+function grid_mask(Ω, L::Tuple{Int,Int})
+    grid = (0:L[1]-1)/L[1], (0:L[2]-1)/L[2]
+    gridΩrefs = findall(Ω.(grid[1],grid[2]')) # find all grid pts inside Ω
+    grid, gridΩrefs
 end
 
+# Evaluates a 2D Fourier extension at (x,y)
 function (F::FourierExtension2)(x,y)
     nx, ny = div.(size(F.coeffs),2)
     real(sum(F.coeffs[k+nx+1,j+ny+1] * exp(2π*im*(k*x + j*y)) for k = -nx:nx, j =-ny:ny))
 end
 
-function grid_eval(F::FourierExtension2, L)
-    # Evaluates a Fourier extension F.Ω ⊂ [0,1]x[0,1]
-    # with coefficients F.coeffs at the grid points F.Ω ∩ ((0:L1)/L1)×((0:L2)/L2)
+# Evaluates a 2D Fourier extension at the grid points F.Ω ∩ ((0:L1)/L1)×((0:L2)/L2)
+function grid_eval(F::FourierExtension2, L::Tuple{Int,Int})
     nx, ny = div.(size(F.coeffs),2)
     Lx, Ly = L
     @assert (Lx ≥ 2nx+1) & (Ly ≥ 2ny+1)
-    c = F.coeffs
-    padded_data = zeros(eltype(c),L)
-    @views padded_data[1:nx+1,1:ny+1] = c[nx+1:2nx+1,ny+1:2ny+1]
-    @views padded_data[1:nx+1,Ly-ny+1:Ly] = c[nx+1:2nx+1,1:ny]
-    @views padded_data[Lx-nx+1:Lx,1:ny+1] = c[1:nx,ny+1:2ny+1]
-    @views padded_data[Lx-nx+1:Lx,Ly-ny+1:Ly] = c[1:nx,1:ny]
+    padded_data = zeros(eltype(F.coeffs),L)
+    @views padded_data[1:nx+1,1:ny+1] = F.coeffs[nx+1:2nx+1,ny+1:2ny+1]
+    @views padded_data[1:nx+1,Ly-ny+1:Ly] = F.coeffs[nx+1:2nx+1,1:ny]
+    @views padded_data[Lx-nx+1:Lx,1:ny+1] = F.coeffs[1:nx,ny+1:2ny+1]
+    @views padded_data[Lx-nx+1:Lx,Ly-ny+1:Ly] = F.coeffs[1:nx,1:ny]
     ifft!(padded_data)
-    x_grid, y_grid, gridrefs = grid_mask(L, F.Ω)
-    vals = real.(padded_data[gridrefs].*sqrt(prod(L)))
-    x_grid, y_grid, gridrefs, vals
+    grid, gridΩrefs = grid_mask(F.Ω, L)
+    vals = real.(padded_data[gridΩrefs]*prod(L))
+    grid, gridΩrefs, vals
 end
 
-function Plots.contourf(F::FourierExtension2, L)
-    x_grid, y_grid, gridrefs, vals = grid_eval(F, L)
-    valsmasked = Matrix{eltype(vals)}(undef,L)*NaN
-    @views valsmasked[gridrefs] .= vals
-    contourf(x_grid, y_grid, valsmasked', aspect_ratio=1, xlabel="x", ylabel = "y")
+function Plots.contourf(F::FourierExtension2, L::Tuple{Int,Int})
+    grid, gridΩrefs, vals = grid_eval(F, L)
+    valsmasked = fill(eltype(vals)(NaN),L)
+    @views valsmasked[gridΩrefs] .= vals
+    contourf(grid[1], grid[2], valsmasked', aspect_ratio=1, xlabel="x", ylabel = "y")
 end
