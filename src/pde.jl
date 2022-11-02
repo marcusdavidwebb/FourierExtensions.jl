7034bcf26913c749a075555e3fdf13293d0d9387
Daan Huybrechs <daan.huybrechs@kuleuven.be>
2022-11-02 23:53:28 +0100

    2d differentiation matrix

--- src/pde.jl
+++ src/pde.jl
@@ -1,9 +1,21 @@
 
 differentiation_matrix(n::Int, order::Int, γ) =
-    Diagonal([π*1im*k/γ for k in -n:n])
+    Diagonal([(π*1im*k/γ)^order for k in -n:n])
 
-function derivative(f::FourierExtension)
+function differentiation_matrix(n::Tuple{Int,Int}, order::Tuple{Int,Int})
+    nx, ny = n
+    d = [(2π*1im*k)^order[1]*(2π*1im*j)^order[2] for k in -nx:nx, j in -ny:ny]
+    Diagonal(d[:])
+end
+
+function derivative(f::FourierExtension, order::Int = 1)
     n = (length(f.coeffs)-1) >> 1
-    D = differentiation_matrix(n, 1, f.γ)
+    D = differentiation_matrix(n, order, f.γ)
     FourierExtension(f.γ, D*f.coeffs)
+end
+
+function derivative(f::FourierExtension2, order::Tuple{Int,Int})
+    n = (size(f.coeffs) .-1) .>> 1
+    D = differentiation_matrix(n, order)
+    FourierExtension2(f.Ω, reshape(D*f.coeffs[:], 2 .*n .+ 1))
 end
